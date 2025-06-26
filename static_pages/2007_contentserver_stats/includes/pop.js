026eb8c9b22b2fd86fae2a4445c7ed1d658d6e6e
Ben Shefte <shefben@gmail.com>
2025-06-25 21:12:30 -0500

    added 2007 contentserver stats page from archive.org

---
+++ static_pages/2007_contentserver_stats/includes/pop.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function popUp(url)
+{
+	newwin = window.open(url,'newwin','width=580,height=220');
+}
+
+
+
+}
+/*
+     FILE ARCHIVED ON 18:42:48 Oct 09, 2007 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:52:26 Jun 25, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.658
+  exclusion.robots: 0.049
+  exclusion.robots.policy: 0.034
+  esindex: 0.012
+  cdx.remote: 48.221
+  LoadShardBlock: 367.599 (3)
+  PetaboxLoader3.datanode: 197.071 (4)
+  PetaboxLoader3.resolve: 264.148 (3)
+  load_resource: 140.473
+*/
