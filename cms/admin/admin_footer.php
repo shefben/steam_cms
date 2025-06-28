251503674aa13b0e5017456ab358bcc3eb9f13b7
shefben <shefben@gmail.com>
2025-06-27 19:10:31 -0500

    Add icon sidebar navigation

--- cms/admin/admin_footer.php
+++ cms/admin/admin_footer.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../db.php';
-$admin_theme = cms_get_setting('admin_theme','default');
+$admin_theme = cms_get_setting('admin_theme','v2');
 $theme_dir = dirname(__DIR__,2)."/themes/{$admin_theme}_admin";
 if(!is_dir($theme_dir)) $theme_dir = dirname(__DIR__,2)."/themes/default_admin";
 include "$theme_dir/footer.php";
