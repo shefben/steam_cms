529b79ffacaabe62d43575d12a3f7a800365771a
Ben Shefte <shefben@gmail.com>
2025-07-05 22:47:16 -0500

    fixed the asset moving scripts to reflect the correct destination for all assets

--- scripts/copy_theme_assets.bat
+++ scripts/copy_theme_assets.bat
@@ -3,11 +3,11 @@ REM Copy archived theme assets into working theme directories
 setlocal
 set ROOT=..\
 
-xcopy /E /Y "%ROOT%archived_steampowered\2005\v1\img" "%ROOT%themes\2005_v1\img"
-xcopy /Y "%ROOT%archived_steampowered\2005\v1\steampowered02.css" "%ROOT%themes\2005_v1\steampowered02.css"
-xcopy /E /Y "%ROOT%archived_steampowered\2005\v2\img" "%ROOT%themes\2005_v2\img"
-xcopy /Y "%ROOT%archived_steampowered\2005\v2\steampowered03.css" "%ROOT%themes\2005_v2\steampowered03.css"
-xcopy /E /Y "%ROOT%archived_steampowered\2004\storefront\img" "%ROOT%themes\2004\storefront\img"
+xcopy /E /Y "%ROOT%archived_steampowered\2005\v1\img" "%ROOT%themes\2005_v1\images"
+xcopy /Y "%ROOT%archived_steampowered\2005\v1\steampowered02.css" "%ROOT%themes\2005_v1\css\steampowered02.css"
+xcopy /E /Y "%ROOT%archived_steampowered\2005\v2\img" "%ROOT%themes\2005_v2\images"
+xcopy /Y "%ROOT%archived_steampowered\2005\v2\steampowered03.css" "%ROOT%themes\2005_v2\css\steampowered03.css"
+xcopy /E /Y "%ROOT%archived_steampowered\2004\storefront\img" "%ROOT%themes\2004\storefront\images"
 xcopy /E /Y "%ROOT%archived_steampowered\2004\storefront\images" "%ROOT%themes\2004\storefront\images"
 xcopy /Y "%ROOT%archived_steampowered\2004\steampowered.css" "%ROOT%themes\2004\css\steampowered.css"
 xcopy /Y "%ROOT%archived_steampowered\2004\steampowered02.css" "%ROOT%themes\2004\css\steampowered02.css"
