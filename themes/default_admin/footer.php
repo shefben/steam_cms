1eb147186dfb318441efbe1105f1a3b4b5eef971
Ben Shefte <shefben@gmail.com>
2025-07-15 17:58:50 -0500

    reverted footer mistakes

--- themes/default_admin/footer.php
+++ themes/default_admin/footer.php
@@ -21,6 +21,16 @@ $(function(){
             $li.fadeOut(150, function(){ $(this).remove(); });
         });
     });
+    $('body').on('click','.help-icon',function(e){
+        e.preventDefault();
+        var txt=$(this).data('help');
+        if(!txt) return;
+        var $m=$('<div class="help-modal" aria-label="Help"><div class="dialog"></div></div>');
+        $m.find('.dialog').text(txt);
+        $('body').append($m);
+        $m.fadeIn(100);
+        $m.on('click',function(){ $m.fadeOut(150,function(){ $m.remove(); }); });
+    });
 });
 </script>
 </body>
