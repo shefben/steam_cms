0bebd0e21f2eb9edc55fd931d7e24a2605023d9f
Ben Shefte <shefben@gmail.com>
2025-08-02 16:44:26 -0500

    moved market messages to correct folder added update news examples

---
+++ archived_steampowered/update_history/2003-2006/platform/update_history/script.js
@@ -0,0 +1,16 @@
+function showBranch(branch){ 
+   var objBranch = document.getElementById(branch); 
+   var sClosed = "[+] " + branch; 
+   var sOpen = "[-] " + branch; 
+   var objTwig = document.getElementById(branch + "-link"); 
+   if(objBranch.style.display=="block") 
+       { 
+       objBranch.style.display="none"; 
+       objTwig.innerHTML = sClosed; 
+       } 
+   else 
+       { 
+       objBranch.style.display="block"; 
+       objTwig.innerHTML = sOpen; 
+       } 
+} 
