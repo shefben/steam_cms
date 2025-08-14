9fe17a9ed125da9aeb076a3d06a4e4e5a76cd83c
shefben <shefben@gmail.com>
2025-08-13 23:24:04 -0500

    chore: add content servers twig templates for 2006-2007 themes

--- content_servers.php
+++ content_servers.php
@@ -10,21 +10,53 @@ $steam_online = check_online($main_ip,$main_port);
 $servers = get_servers($db);
 foreach ($servers as &$srv) {
     $srv['available_bandwidth'] = $srv['total_capacity'];
+    $used = ($srv['total_capacity'] ?? 0) - ($srv['available_bandwidth'] ?? 0);
+    $load = ($srv['total_capacity'] ?? 0) > 0 ? ($used / $srv['total_capacity']) * 100 : 0;
+    $load = max(0, min(100, $load));
+    $srv['load_percent'] = $load;
+    $srv['avail_percent'] = 100 - $load;
 }
+unset($srv);
 $last_update    = date('Y-m-d H:i:s');
 $total_capacity = get_total_capacity($servers);
 $total_available = $total_capacity;
-$theme  = cms_get_setting('theme', '2004');
-$theme_dir = __DIR__ . '/themes/' . $theme;
-if (!is_file($theme_dir.'/contentserver_block.php')) {
-    $theme_dir = __DIR__.'/themes/2004';
-    $theme = '2004';
-}
-$theme_file = $theme_dir.'/contentserver_block.php';
 
-ob_start();
-include $theme_file;
-$content = ob_get_clean();
+$regions = [];
+foreach ($servers as $srv) {
+    $reg = $srv['region'] ?? 'Unknown';
+    $regions[$reg][] = $srv;
+}
+ksort($regions, SORT_NATURAL | SORT_FLAG_CASE);
+foreach ($regions as &$list) {
+    usort($list, fn($a, $b) => strcasecmp($a['name'], $b['name']));
+}
+unset($list);
+
+$theme  = cms_get_setting('theme', '2004');
+$template = cms_theme_page_template('content_servers', $theme);
+if (basename($template) === 'content_servers.twig') {
+    $tplDir = dirname($template);
+    $content = cms_render_string(
+        file_get_contents($template),
+        [
+            'servers' => $servers,
+            'regions' => $regions,
+            'total_available' => $total_available,
+            'total_capacity' => $total_capacity,
+        ],
+        $tplDir
+    );
+} else {
+    $theme_dir = __DIR__ . '/themes/' . $theme;
+    if (!is_file($theme_dir.'/contentserver_block.php')) {
+        $theme_dir = __DIR__.'/themes/2004';
+        $theme = '2004';
+    }
+    $theme_file = $theme_dir.'/contentserver_block.php';
+    ob_start();
+    include $theme_file;
+    $content = ob_get_clean();
+}
 
 $tpl = cms_theme_layout('default.twig', $theme);
 cms_render_template_theme($tpl, $theme, [
