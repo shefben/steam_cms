28ad486447cba4208562c9a918f37e5da3e1a989
Ben Shefte <shefben@gmail.com>
2025-08-17 13:08:14 -0500

    added more news pages

---
+++ news_pages/news_413%.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+
+<head>
+	<title>Steam News</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta name="ROBOTS" content="ALL">
+	<meta name="DESCRIPTION" content="SteamPowered">
+	<meta name="KEYWORDS" content="Steam, account, account creation, signup">
+	<meta name="AUTHOR" content="Valve LLC">
+	<LINK rel="stylesheet" type="text/css" href="steampowered.css">
+	<link rel="Shortcut Icon" type="image/png" href="/webicon.png">
+	<script language="JavaScript" src="nav.js"></script>
+	<link href="http://www.steampowered.com/rss.xml" rel="alternate" type="application/rss+xml" title="Valve RSS News Feed" />
+</head>
+
+<body>
+
+<!-- begin header -->
+<div class="header">
+<nobr><a href="index.php"><img src="/img/steam_logo_onblack.gif" align="top" alt="[Steam]" height="54" width="152"></a>
+<span class="navBar">
+	<a href="index.php?area=news"><img name="news" valign="bottom" height="22" width="54" src="img/news.gif"
+		onMouseOut="this.src='img/news.gif';" onMouseOver="this.src='img/MOnews.gif';"
+		alt="news"></a>
+
+	<a href="index.php?area=getsteamnow"><img valign="bottom" height="22" width="108" src="img/getSteamNow.gif"
+		onMouseOut="this.src='img/getSteamNow.gif'" onMouseOver="this.src='img/MOgetSteamNow.gif'"
+		alt="getSteamNow"></a>
+
+	<a href="index.php?area=cybercafes"><img valign="bottom" height="22" width="95" src="img/cafes.gif"
+		onMouseOut="this.src='img/cafes.gif'" onMouseOver="this.src='img/MOcafes.gif'"
+		alt="Cyber Cafes"></a>
+
+	<a href="http://steampowered.custhelp.com/"><img valign="bottom" height="22" width="68" src="img/support.gif"
+		onMouseOut="this.src='img/support.gif'" onMouseOver="this.src='img/MOsupport.gif'"
+		alt="Support"></a>
+
+	<a href="index.php?area=forums"><img valign="bottom" height="22" width="68" src="img/forums.gif"
+		onMouseOut="this.src='img/forums.gif'" onMouseOver="this.src='img/MOforums.gif'"
+		alt="Forums"></a>
+
+	<a href="status/status.html"><img valign="bottom" height="22" width="65" src="img/status.gif"
+		onMouseOut="this.src='img/status.gif'" onMouseOver="this.src='img/MOstatus.gif'"
+		alt="Status"></a>
+</span>
+</nobr>
+</div>
+<!-- end header -->
+
+
+	<!-- news -->
+
+	<div class="content" id="container">
+	<h1>STEAM NEWS</H1>
+	<h2>VALVE <em>NEWS</em></h2><img src="/img/Graphic_box.jpg" height="6" width="24" alt=""><br>
+	<br>
+	<div class="narrower">
+	<p><h3><a href="index.php?area=news&id=413" style="text-decoration: none; color: #BFBA50;">Valve and Vivendi Universal Games Settle Lawsuit</a></h3>
+<span style="font-size: 9px;">April 29, 2005, 9:47 am &middot; <a href="mailto:contact@valvesoftware.com" style="text-decoration: none;">Chris Bokitch</a><table width="100%" cellpadding="0" cellspacing="0"><tr><td height="1" width="100%" bgcolor="#808080"></td></tr><tr><td height="10" width="100%"></td></tr></table></span>
+Bellevue, WA and Los Angeles, CA - April 29, 2005 -- Valve and Vivendi Universal Games (VU Games) today announced the settlement of a pending federal court lawsuit filed by Valve in August 2002.  The parties have resolved their differences, and the settlement provides for the dismissal of all claims and counterclaims.  Under the settlement agreement, VU Games will cease distribution of retail packaged versions of Valve's games, including Half-Life®, Half-Life 2, Counter-Strike™, Counter-Strike: Condition Zero and Counter-Strike: Source, effective August 31, 2005.<br><br>Additionally, VU Games has notified distributors and cyber cafés that were licensed by VU Games that only Valve is authorized to distribute Valve games to cyber cafés and grant cyber café licenses.  Cyber café operators that were licensed by VU Games have also been notified that any license agreement from Sierra Entertainment, Vivendi Universal Games or any of their affiliates or distributors that may have granted rights to use Valve games in cyber cafés, whether written or oral, is terminated.<br><br>About Valve<br>Based in Bellevue, Washington, Valve is an entertainment software and technology company founded in 1996. Valve's debut title, Half-Life, has won over 50 Game of the Year Awards and named "Best PC Game Ever" in the November 1999, October 2001, and April 2005 issues of PC Gamer, the world's best-selling PC games magazine. Valve's portfolio of entertainment titles also includes Counter-Strike, Day of Defeat™, and Team Fortress® and accounts for over 15 million retail units sold worldwide, and over 88% of the PC online action market.  Information about Valve's Cyber Café  Program can be found at <a href="http://www.steampowered.com/">www.steampowered.com</a>.<br><br>Vivendi Universal Games (VU Games)<br>Vivendi Universal Games (<a href="http://www.vugames.com/">www.vugames.com</a>) is a global developer, publisher and distributor of multi-platform interactive entertainment. The company is a leader in the subscription-based massively multi-player online (MMO) games category, and also holds leading positions in the PC, console and handheld games markets. Its development studios and publishing labels include Blizzard Entertainment®, Radical Entertainment™, Sierra® Entertainment and Massive Entertainment™. VU Games' library of over 700 titles features owned intellectual properties including Warcraft®, StarCraft®, Diablo® and World of Warcraft® from Blizzard; Crash Bandicoot®, Spyro The Dragon™, Empire Earth®, Leisure Suit Larry™, Ground Control® and Tribes®. VU Games also maintains strategic relationships with industry leading content partners, including NBC Universal and Twentieth Century Fox.<br><br><b><em>Press contacts:</em></b><br><br>Doug Lombardi<br>Valve<br>425.889.9642 x160<br><a href="mailto:lombardi@valvesoftware.com">lombardi@valvesoftware.com</a><br><br>Leslie Hollingshead<br>Vivendi Universal Games<br>310.431.4533<br><a href="mailto:leslie.hollingshead@vugames.com">leslie.hollingshead@vugames.com</a><br></p>
+
+<ul><li> <a href="index.php?area=news" style="text-decoration: none;"><i>return to news page</i></a></ul>
+	</div>
+	</div>
+	
+
+<!-- begin footer -->
+<div class="footer">
+	<table cellpadding="0" cellspacing="0">
+	<tr>
+		<td><a href="http://www.valvesoftware.com"><img src="img/valve_greenlogo.gif"></a></td>
+		<td>&nbsp;</td>
+		<td><span class="footerfix">&copy; 2005 Valve Corporation. All rights reserved. Valve, the Valve logo, Half-Life, the Half-Life logo, the Lambda logo, Steam, the Steam logo, Team Fortress, the Team Fortress logo, Opposing Force, Day of Defeat, the Day of Defeat logo, Counter-Strike, the Counter-Strike logo, Source, the Source logo, Valve Source and Counter-Strike: Condition Zero are trademarks and/or registered trademarks of Valve Corporation. <a href="http://www.valvesoftware.com/privacy.htm">Privacy Policy</a>. <a href="http://www.valvesoftware.com/legal.htm">Legal</a>. <a href="/index.php?area=subscriber_agreement">Steam Subscriber Agreement</a>.</span></td>
+		<td width="15%">&nbsp;</td>
+	</tr>
+	</table>
+</div>
+<!-- end footer -->
+
+</body>
+</html>
